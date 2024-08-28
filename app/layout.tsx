32c54a141604f68c40ef9b36889b47eeb6c54860
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-08-28 15:43:08 +0330

    added divider of hero and other sectins in home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({children}: RootLayoutType): ReactNode {
     return (
         <html>
             <body
-                className={cn('bg-darkTheme', iranYekanFont.className)}
+                className={cn('bg-darkTheme overflow-x-hidden', iranYekanFont.className)}
                 dir={'rtl'}
             >
                 <Header/>
